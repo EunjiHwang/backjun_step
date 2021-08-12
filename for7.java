71e33883c95d51d686be75f3eb78ac67f911991a
EunjiHwang <1101j@naver.com>
2021-08-12 14:12:25 +0900

    for문 - 7 A+B-7

---
+++ for7.java
@@ -0,0 +1,28 @@
+// A+B-7
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		int sum = 0;
+		
+		for(int i=1; i<=T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			sum = A+B;
+			
+			bw.write("Case #"+ i + ": " + sum + "\n");
+		}
+		bw.flush(); // 남아있는 데이터 모두 출력
+	}
+
+}
